76aec5a930e467d154693428cb47986c37178221
Gaurav Chaudhari <deigue@Hotmail.com>
2025-08-15 03:23:21 -0400

    Mark tests package for app

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Test package for the folio-updater application.
+
+This package contains all the test modules for the application.
+"""
+
+# This file makes the tests directory a Python package, allowing test discovery
+# and proper imports across test modules.
