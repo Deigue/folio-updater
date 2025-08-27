bb62b962d2984fe9667911271fc08592746ede87
Gaurav Chaudhari <deigue@Hotmail.com>
2025-08-27 03:42:05 -0400

    Use enum literals in mock_data.py

--- src/mock_data.py
+++ src/mock_data.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta, timezone
 
 import pandas as pd
 
-from src.constants import TXN_ESSENTIALS
+from src.constants import TXN_ESSENTIALS, Action, Column, Currency
 
 logger = logging.getLogger(__name__)
 
@@ -28,19 +28,19 @@ def generate_transactions(ticker: str, num_transactions: int = 5) -> pd.DataFram
         txn_date = (end_date - timedelta(days=num_transactions * 30)).strftime(
             "%Y-%m-%d",
         )
-        action: str = random.choice(["BUY", "SELL"])  # noqa: S311
-        currency = "USD"
+        action = random.choice([Action.BUY, Action.SELL])  # noqa: S311
+        currency = Currency.USD
         price = round(random.uniform(10, 500), 2)  # noqa: S311
         units = round(random.uniform(1, 100), 2)  # noqa: S311
 
         transaction = {
-            "TxnDate": txn_date,
-            "Action": action,
-            "Amount": price * units,
-            "$": currency,
-            "Price": price,
-            "Units": units,
-            "Ticker": ticker,
+            Column.Txn.TXN_DATE: txn_date,
+            Column.Txn.ACTION: action,
+            Column.Txn.AMOUNT: price * units,
+            Column.Txn.CURRENCY: currency,
+            Column.Txn.PRICE: price,
+            Column.Txn.UNITS: units,
+            Column.Txn.TICKER: ticker,
         }
         transactions.append(transaction)
 
