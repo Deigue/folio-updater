4e4d3de2d1086cdb973505c5662535902cc56bc0
Gaurav Chaudhari <deigue@Hotmail.com>
2025-08-27 03:40:21 -0400

    Add customizable enums to constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,17 +1,55 @@
-"""
-Non-user-configurable defaults/constants used throughout the app.
-"""
-
+"""Non-user-configurable defaults/constants used throughout the app."""
 
 # Internal transaction fields that are essential for processing
+from __future__ import annotations
+
+from enum import Enum
+
+
+class AutoStrEnum(Enum):
+    """Enum that automatically creates a string representation of the enum value."""
+
+    def __str__(self):
+        return self.value
+
+
+class Currency(AutoStrEnum):
+    """Currency codes."""
+
+    USD = "USD"
+    CAD = "CAD"
+
+
+class Action(AutoStrEnum):
+    """Transaction actions."""
+
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class Column(AutoStrEnum):
+    """Constants for column names."""
+
+    class Txn(AutoStrEnum):
+        """Transaction columns."""
+
+        TXN_DATE = "TxnDate"
+        ACTION = "Action"
+        AMOUNT = "Amount"
+        CURRENCY = "$"
+        PRICE = "Price"
+        UNITS = "Units"
+        TICKER = "Ticker"
+
+
 TXN_ESSENTIALS = [
-    "TxnDate",  # Date of transaction
-    "Action",   # BUY/SELL
-    "Amount",   # Total amount (Price * Units)
-    "$",        # Currency
-    "Price",    # Price per unit
-    "Units",    # Number of units
-    "Ticker"    # Stock or ETF ticker
+    Column.Txn.TXN_DATE,  # Date of transaction
+    Column.Txn.ACTION,  # BUY/SELL
+    Column.Txn.AMOUNT,  # Total amount (Price * Units)
+    Column.Txn.CURRENCY,  # Currency
+    Column.Txn.PRICE,  # Price per unit
+    Column.Txn.UNITS,  # Number of units
+    Column.Txn.TICKER,  # Stock or ETF ticker
 ]
 
 # Default tickers for newly created folio file
