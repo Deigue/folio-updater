5abff9fb1b6f2deb0a02297616c755da2beb7f70
Gaurav Chaudhari <deigue@Hotmail.com>
2025-08-28 04:47:43 -0400

    Fixup for AutoStrEnum related definitions

--- src/constants.py
+++ src/constants.py
@@ -3,15 +3,57 @@
 # Internal transaction fields that are essential for processing
 from __future__ import annotations
 
+import sqlite3
 from enum import Enum
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from types import NotImplementedType
 
 
 class AutoStrEnum(Enum):
     """Enum that automatically creates a string representation of the enum value."""
 
+    def __init__(self, *args: object) -> None:
+        """Override base Enum initialization to rep with a string."""
+        super().__init__(*args)
+        self._value_ = str(self._value_)
+
     def __str__(self) -> str:
         """Return a string representation of the enum value."""
         return self.value
+
+    def __conform__(self, protocol: sqlite3.PrepareProtocol) -> str:
+        """Conform to string for sqlite3."""
+        if protocol is sqlite3.PrepareProtocol:
+            return self.value
+        msg = f"Cannot conform {self} to {protocol}"  # pragma: no cover
+        raise TypeError(msg)  # pragma: no cover
+
+    def __hash__(self) -> int:
+        """Hash the enum value."""
+        return hash(self.value)
+
+    def __eq__(self, other: object) -> bool | NotImplementedType:
+        """Compare the enum value to another enum or a string.
+
+        If the `other` argument is an instance of `AutoStrEnum`, compare the
+        underlying values.
+
+        If the `other` argument is a string, compare the string representations
+        of the enum values.
+
+        If the `other` argument is any other type, return `NotImplemented`.
+        """
+        if isinstance(other, AutoStrEnum):
+            return self.value == other.value  # pragma: no cover
+        if isinstance(other, str):
+            return self.value == other
+        return NotImplemented  # pragma: no cover
+
+    def __repr__(self) -> str:
+        """Representation of this enum should be a string."""
+        return self.value  # pragma: no cover
 
 
 class Currency(AutoStrEnum):
@@ -28,6 +70,12 @@ class Action(AutoStrEnum):
     SELL = "SELL"
 
 
+class Table(AutoStrEnum):
+    """Table names."""
+
+    TXNS = "Txns"
+
+
 class Column(AutoStrEnum):
     """Constants for column names."""
 
@@ -42,15 +90,23 @@ class Column(AutoStrEnum):
         UNITS = "Units"
         TICKER = "Ticker"
 
+    class Ticker(AutoStrEnum):
+        """Ticker columns."""
 
-TXN_ESSENTIALS = [
-    Column.Txn.TXN_DATE,  # Date of transaction
-    Column.Txn.ACTION,  # BUY/SELL
-    Column.Txn.AMOUNT,  # Total amount (Price * Units)
-    Column.Txn.CURRENCY,  # Currency
-    Column.Txn.PRICE,  # Price per unit
-    Column.Txn.UNITS,  # Number of units
-    Column.Txn.TICKER,  # Stock or ETF ticker
+        TICKER = "Ticker"
+
+
+TXN_ESSENTIALS: list[str] = [
+    str(col)
+    for col in [
+        Column.Txn.TXN_DATE,  # Date of transaction
+        Column.Txn.ACTION,  # BUY/SELL
+        Column.Txn.AMOUNT,  # Total amount (Price * Units)
+        Column.Txn.CURRENCY,  # Currency
+        Column.Txn.PRICE,  # Price per unit
+        Column.Txn.UNITS,  # Number of units
+        Column.Txn.TICKER,  # Stock or ETF ticker
+    ]
 ]
 
 # Default tickers for newly created folio file
