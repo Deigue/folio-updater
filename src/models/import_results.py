721cfaa4860666e283cd37c6d1e00343e35b546c
Gaurav Chaudhari <deigue@Hotmail.com>
2025-11-21 18:41:03 -0500

    Track transformation events (refs #62)

--- src/models/import_results.py
+++ src/models/import_results.py
@@ -19,9 +19,26 @@ class MergeEvent:
 
     Attributes:
         merged_row: The resulting merged transaction (as Series converted to dict)
-        source_rows: Original source transactions merged (DataFrame limited copy)
+        source_rows: Original source transactions that were merged (as DataFrame)
     """
 
     merged_row: dict[str, Any]
     source_rows: pd.DataFrame
+
+
+@dataclass
+class TransformEvent:
+    """Represents a transformation applied to a set of rows.
+
+    Attributes:
+        field_name: Name of the field transformed
+        old_values: Distinct original values before transform
+        new_value: The new value applied
+        row_count: Number of rows affected
+    """
+
+    field_name: str
+    old_values: list[Any]
+    new_value: Any
+    row_count: int
 
